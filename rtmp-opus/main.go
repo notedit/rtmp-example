b989aecc0da8aaa21ff954b630a3176f9e460ce1
notedit <notedit@gmail.com>
2020-05-24 19:25:26 +0800

    test opus

--- rtmp-opus/main.go
+++ rtmp-opus/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/notedit/gst"
 	"github.com/notedit/rtmp/av"
 	"github.com/notedit/rtmp/codec/h264"
+	"github.com/notedit/rtmp/codec/opus"
 	"github.com/notedit/rtmp/format/rtmp"
 	"net"
 	"time"
@@ -18,24 +19,97 @@ type stream struct {
 
 var pub *stream
 
-const gstPipeline = "videotestsrc ! video/x-raw,framerate=15/1 ! x264enc aud=false bframes=0 speed-preset=veryfast key-int-max=15 ! video/x-h264,stream-format=avc,profile=baseline ! h264parse ! appsink name=videosink "
+const gstVideoPipeline = "videotestsrc ! video/x-raw,framerate=15/1 ! x264enc aud=false bframes=0 speed-preset=veryfast key-int-max=30 ! video/x-h264,stream-format=avc,profile=baseline ! h264parse ! appsink name=videosink "
+const gstAudioPipeline = "audiotestsrc wave=sine ! audio/x-raw, rate=48000, channels=2 ! audioconvert ! opusenc ! appsink name=audiosink"
+
+type Sample struct {
+	Data     []byte
+	Video    bool
+	Duration uint64
+}
+
+func pullVideoSample(element *gst.Element, out chan *Sample) {
+
+	for {
+
+		sle, err := element.PullSample()
+		if err != nil {
+			if element.IsEOS() == true {
+				fmt.Println("eos")
+				return
+			} else {
+				fmt.Println(err)
+				continue
+			}
+		}
+
+		sample := &Sample{
+			Data:     sle.Data,
+			Video:    true,
+			Duration: sle.Duration,
+		}
+
+		out <- sample
+	}
+}
+
+func pullAudioSample(element *gst.Element, out chan *Sample) {
+
+	for {
+
+		sle, err := element.PullSample()
+		if err != nil {
+			if element.IsEOS() == true {
+				fmt.Println("eos")
+				return
+			} else {
+				fmt.Println(err)
+				continue
+			}
+		}
+
+		sample := &Sample{
+			Data:     sle.Data,
+			Video:    false,
+			Duration: sle.Duration,
+		}
+
+		out <- sample
+	}
+
+}
 
 func startPush() {
 
-	err := gst.CheckPlugins([]string{"x264", "videoparsersbad"})
+	samples := make(chan *Sample, 10)
+
+	err := gst.CheckPlugins([]string{"x264", "videoparsersbad", "opus"})
 
 	if err != nil {
 		panic(err)
 	}
 
-	pipeline, err := gst.ParseLaunch(gstPipeline)
+	vpipeline, err := gst.ParseLaunch(gstVideoPipeline)
 
 	if err != nil {
 		panic(err)
 	}
 
-	element := pipeline.GetByName("videosink")
-	pipeline.SetState(gst.StatePlaying)
+	velement := vpipeline.GetByName("videosink")
+	vpipeline.SetState(gst.StatePlaying)
+
+	go pullVideoSample(velement, samples)
+
+	apipeline, err := gst.ParseLaunch(gstAudioPipeline)
+
+	if err != nil {
+		panic(err)
+	}
+
+	aelement := apipeline.GetByName("audiosink")
+	apipeline.SetState(gst.StatePlaying)
+
+	go pullAudioSample(aelement, samples)
 
 	rtmpClient := rtmp.NewClient()
 
@@ -52,63 +126,72 @@ func startPush() {
 		IsKeyFrame: true,
 	}
 
-	start := time.Now()
-
-	for {
-		sample, err := element.PullSample()
-		if err != nil {
-			if element.IsEOS() == true {
-				fmt.Println("eos")
-				return
-			} else {
-				fmt.Println(err)
-				continue
-			}
-		}
-
-		nalus, _ := h264.SplitNALUs(sample.Data)
-		nalus_ := make([][]byte, 0)
-		keyframe := false
-		for _, nalu := range nalus {
-			typ := h264.NALUType(nalu)
-			switch typ {
-			case h264.NALU_SPS:
-				h264Codec.AddSPSPPS(nalu)
-			case h264.NALU_PPS:
-				h264Codec.AddSPSPPS(nalu)
-				decodeConfig.Data = make([]byte, 2048)
-				var len int
-				h264Codec.ToConfig(decodeConfig.Data, &len)
-				decodeConfig.Data = decodeConfig.Data[0:len]
-				c.WritePacket(decodeConfig)
-			case h264.NALU_IDR:
-				nalus_ = append(nalus_, nalu)
-			case h264.NALU_NONIDR:
-				nalus_ = append(nalus_, nalu)
-				keyframe = true
-				break
-			}
-			//fmt.Println("client ",h264.NALUTypeString(h264.NALUType(nalu)))
-		}
-
-		duration := time.Since(start)
-		data := h264.FillNALUsAVCC(nalus_)
-		pkt := av.Packet{
-			Type: av.H264,
-			IsKeyFrame:keyframe,
-			Time: duration,
-			CTime: duration,
-			Data: data,
-		}
-
-		c.WritePacket(pkt)
-		//fmt.Println("client ",pkt)
+	opusCodec := &opus.Codec{
+		SampleRate: 48000,
+		Channels:   2,
 	}
 
-	pipeline.SetState(gst.StateNull)
+	start := time.Now()
 
-	pipeline = nil
-	element = nil
+	for sample := range samples {
+
+		if sample.Video {
+			nalus, _ := h264.SplitNALUs(sample.Data)
+			nalus_ := make([][]byte, 0)
+			keyframe := false
+			for _, nalu := range nalus {
+				typ := h264.NALUType(nalu)
+				switch typ {
+				case h264.NALU_SPS:
+					h264Codec.AddSPSPPS(nalu)
+				case h264.NALU_PPS:
+					h264Codec.AddSPSPPS(nalu)
+					decodeConfig.Data = make([]byte, 2048)
+					var len int
+					h264Codec.ToConfig(decodeConfig.Data, &len)
+					decodeConfig.Data = decodeConfig.Data[0:len]
+					c.WritePacket(decodeConfig)
+				case h264.NALU_IDR:
+					nalus_ = append(nalus_, nalu)
+				case h264.NALU_NONIDR:
+					nalus_ = append(nalus_, nalu)
+					keyframe = true
+					break
+				}
+			}
+
+			duration := time.Since(start)
+			data := h264.FillNALUsAVCC(nalus_)
+			pkt := av.Packet{
+				Type:       av.H264,
+				IsKeyFrame: keyframe,
+				Time:       duration,
+				CTime:      duration,
+				Data:       data,
+			}
+			c.WritePacket(pkt)
+
+		} else {
+
+			duration := time.Since(start)
+			pkt := av.Packet{
+				Type:  av.OPUS,
+				Time:  duration,
+				CTime: duration,
+				Data:  sample.Data,
+				OPUS:  opusCodec,
+			}
+			c.WritePacket(pkt)
+		}
+
+	}
+
+	vpipeline.SetState(gst.StateNull)
+	apipeline.SetState(gst.StateNull)
+
+	vpipeline = nil
+	velement = nil
+
 }
 
 func startRtmp() {
@@ -146,11 +229,13 @@ func startRtmp() {
 				pub.videoSeq = pkt
 				fmt.Println("server  sps  pps ========")
 			case av.H264:
-				fmt.Println("server ", pkt.Time)
+				fmt.Println("server h264 ", pkt.Time)
 			case av.AACDecoderConfig:
 				pub.audioSeq = pkt
 			case av.AAC:
-				fmt.Println("server ", pkt.Time)
+				fmt.Println("server aac", pkt.Time)
+			case av.OPUS:
+				fmt.Println("server opus", pkt.Time)
 			}
 		}
 	}
